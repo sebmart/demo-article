ab405b1bed3f14c8e2c4c438a2b057ec7f0ddf2d
Sebastien Martin <92sebastien@gmail.com>
2014-05-29 16:35:57 -0700

    Demo is linked with ramp-metering

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,30 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import edu.berkeley.path.ramp_metering._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val n = 6
+    val t = 10
+    val onrampLocation = (n/2).toInt + 1
+    val link = FreewayLink(FundamentalDiagram(1.0, 1.0, 2.0), 1.0, 1.0, 4.0)
+    val fw = Freeway(IndexedSeq.fill(6)(link), IndexedSeq(0, onrampLocation), IndexedSeq())
+    val queues = Array.fill(n)(0.0)
+    queues(0) = 1.0
+    queues(onrampLocation) = 1.0
+    val ic = InitialConditions(IndexedSeq.fill(n)(1.0), queues.toIndexedSeq)
+    val flows = IndexedSeq.fill(t)(IndexedSeq(1.0, 0.2))
+
+    val bc = BoundaryConditions(flows, IndexedSeq.fill(t, 0)(1.0))
+    val simParams = SimulationParameters(bc, ic)
+    val scen = FreewayScenario(fw, simParams, PolicyParameters(1))
+    //    val scen =LoadScenario.loadScenario("data/networks/samitha1onramp.json")
+    println(scen)
+    println(FreewaySimulator.simpleSim(scen))
+    println(FreewaySimulator.simpleSim(scen, Array.fill(t)(0.0)))
+    Ok("Hello world !!")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -2,10 +2,18 @@ name := "demo"
 
 version := "1.0-SNAPSHOT"
 
+resolvers += Resolver.file("Local ivy Repository", file(Path.userHome.absolutePath + "/.ivy2/local/"))(Resolver.ivyStylePatterns)
+
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache
-)     
+  "com.typesafe.play" %% "play-json" % "2.2.3",
+  "org.scalatest" %% "scalatest" % "1.9.2" % "test",
+  "org.apache.commons" % "commons-math3" %  "3.1",
+  "net.sourceforge.parallelcolt" % "parallelcolt" % "0.10.0",
+  "org.scalaj" % "scalaj-time_2.10.2" % "0.7",
+  "org.mongodb" %% "casbah" % "2.6.3",
+  "com.novus" %% "salat" % "1.9.2",
+  "edu.berkeley.path" % "ramp-metering_2.10" % "0.102-SNAPSHOT",
+  "ch.qos.logback" % "logback-classic" % "1.0.6"
+)
 
 play.Project.playScalaSettings
